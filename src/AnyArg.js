1eb62f5b707cf33a5a99a3b60ac1787e5c8e284a
mahdi-farnia <65429393+mahdi-farnia@users.noreply.github.com>
2020-10-06 00:02:48 +0330

    accept multiple type of entries

--- src/AnyArg.js
+++ src/AnyArg.js
@@ -1,9 +1,53 @@
+const { isPlainObject } = require('is-plain-object');
+
 class AnyArg {
   /**
-   * @param {Function} task
+   * @param { Function | Function[] | { [key: string]: Function } } task
    */
   constructor(task) {
-    task.constructor = AnyArg;
+    /**
+     * @private { Function | Function[] | { [key: string]: Function } }
+     */
+    this._task = null;
+
+    if (isPlainObject(task)) {
+      for (const functionName in task) {
+        const fn = task[functionName];
+
+        if (typeof fn !== 'function') continue;
+
+        fn.constructor = AnyArg;
+      }
+      this._task = task;
+    } else if (Array.isArray(task)) {
+      // Use <for> loop for using continue;
+      var i = 0,
+        len = task.length;
+
+      for (; i < len; i++) {
+        const fn = task[i];
+
+        if (typeof fn !== 'function') continue;
+
+        fn.constructor = AnyArg;
+      }
+
+      this._task = task;
+    } else if (typeof task === 'function') {
+      task.constructor = AnyArg;
+      this._task = task;
+    } else {
+      throw new TypeError(
+        '\ntask argument should be plain object of function or array of function or function\n'
+      );
+    }
+  }
+
+  /**
+   * @returns { Function | Function[] | { [key: string]: Function } }
+   */
+  get task() {
+    return this._task;
   }
 
   static isThis(arg) {
